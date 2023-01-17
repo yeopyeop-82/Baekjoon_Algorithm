e3b9c27ed559c6559fe4766fc134bfaf189b9173
Seung-yeop Kim <61226778+yeopyeop-82@users.noreply.github.com>
2023-01-17 09:55:34 +0900

    [Bronze V] Title: 검증수, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int sum = 0;
+        int temp = 0;
+
+        while(st.hasMoreTokens()) {
+            temp = (Integer.parseInt(st.nextToken()));
+            sum += (temp * temp);
+        }
+
+        System.out.println(sum % 10);
+    }
+}
