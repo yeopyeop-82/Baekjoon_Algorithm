869a711089bbb3a95dd03a44cb3f7a9393ca5944
Seung-yeop Kim <61226778+yeopyeop-82@users.noreply.github.com>
2023-01-17 09:55:46 +0900

    [Bronze III] Title: 최댓값, Time: 212 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] arr = new int[9];
+        int max = 0;
+        int maxIndex = 0;
+        
+        for (int i = 0; i < 9; i++) {
+            arr[i] = s.nextInt();
+            if (arr[i] >= max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(maxIndex + 1);
+
+        s.close();
+    }
+}
