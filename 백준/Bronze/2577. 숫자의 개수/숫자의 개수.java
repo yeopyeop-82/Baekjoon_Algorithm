ca88947dd92a8a456f08317ff0dad8d82a094df6
Seung-yeop Kim <61226778+yeopyeop-82@users.noreply.github.com>
2023-01-17 09:55:55 +0900

    [Bronze II] Title: 숫자의 개수, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = 1;
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            result *= s.nextInt();
+        }
+        s.close();
+
+        String stringResult = Integer.toString(result);
+        int[] resultArr = new int[10];
+
+        for (int j = 0; j < stringResult.length(); j++) {
+            switch (stringResult.charAt(j)) {
+                case '0':
+                resultArr[0] += 1;
+                break;
+
+                case '1':
+                resultArr[1] += 1;
+                break;
+
+                case '2':
+                resultArr[2] += 1;
+                break;
+
+                case '3':
+                resultArr[3] += 1;
+                break;
+
+                case '4':
+                resultArr[4] += 1;
+                break;
+
+                case '5':
+                resultArr[5] += 1;
+                break;
+
+                case '6':
+                resultArr[6] += 1;
+                break;
+
+                case '7':
+                resultArr[7] += 1;
+                break;
+
+                case '8':
+                resultArr[8] += 1;
+                break;
+
+                case '9':
+                resultArr[9] += 1;
+                break;
+            }
+        }
+        
+        for (int k = 0; k < 10; k++) {
+            System.out.println(resultArr[k]);
+        }
+    }
+}
