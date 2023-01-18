9cdb03fcd8f35aae70d342b2de1333ae6a5e414d
Seung-yeop Kim <61226778+yeopyeop-82@users.noreply.github.com>
2023-01-18 23:16:05 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string s[5];
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> s[i];
+	}
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (i < s[j].size())
+			{
+				cout << s[j][i];
+			}
+		}
+	}
+	return 0;
+}
