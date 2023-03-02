a348b8c5348e51905f6fe63052d3e114d9f67ecd
Seung-yeop Kim <61226778+yeopyeop-82@users.noreply.github.com>
2023-03-02 14:30:00 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] square = new int[1001];
+        
+        square[1] = 1;
+        square[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            square[i] = (square[i - 1] + 2 * square[i - 2]) % 10007;
+        }
+        System.out.println(square[n]);
+    }
+}
